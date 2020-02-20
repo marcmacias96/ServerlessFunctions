ef9871e520f9faebf1cb4156c64de4a7e7feca7d
Luis Alejandro De la A Gonzalez <luis_delaa@registropmanta.local>
2020-02-20 16:15:46 -0500

    add

--- querys/typesOfProcedure.js
+++ querys/typesOfProcedure.js
@@ -1,6 +1,6 @@
 module.exports = {
     tiposTramite : `query rep_tipoTramite($fechaInicio: timestamp!, $fechaFin: timestamp!, $title: String, $order: order_by!) {
-      OrdenTrabajo_Cabecera(where: {FechaOT: {_lt: $fechaFin, _gt: $fechaInicio}, Estado: {_eq: 3}}) {
+      OrdenTrabajo_Cabecera(where: {FechaOT: {_lt: $fechaFin, _gt: $fechaInicio}, Estado: {_gt: 2, _lt:8}}) {
         OrdenTrabajo_Detalles(where: {TipoServicio: {TpServicio: {_neq: "ZERROR"}}, TipoTramite: {DscaTipoTramite: {_ilike: $title}}}, order_by: {TipoTramite: {DscaTipoTramite: $order}}) {
           TipoTramite {
             DscaTipoTramite
