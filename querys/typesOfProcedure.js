d66c5951abfef9e1e5cb81e85ca77b403720038c
marcmacias96 <mamacias3@espe.edu.ec>
2020-02-20 17:30:24 -0500

    fix query

--- querys/typesOfProcedure.js
+++ querys/typesOfProcedure.js
@@ -1,8 +1,7 @@
 module.exports = {
-    tiposTramite : `query rep_tipoTramite($fechaInicio: timestamp!, $fechaFin: timestamp!) {
+    tiposTramite : `query rep_tipoTramite($fechaInicio: timestamp!, $fechaFin: timestamp!, $title: String, $order: String!) {
       OrdenTrabajo_Cabecera (where: {FechaOT: {_lt: $fechaFin, _gt: $fechaInicio}, _or: [{Estado: {_eq: 3}}, {Estado:{_eq: 7}},{_and:[{Estado:{_eq: 5}}, {OrdenPostPago:{_eq:false}}]} ]}) {
-        OrdenPostPago
-        OrdenTrabajo_Detalles{
+        OrdenTrabajo_Detalles(where: {TipoServicio: {TpServicio: {_neq: "ZERROR"}}, TipoTramite: {DscaTipoTramite: {_ilike: $title}}}, order_by: {TipoTramite: {DscaTipoTramite: $order}}){
           TipoTramite {
             DscaTipoTramite
           }
@@ -10,6 +9,5 @@ module.exports = {
         }
       }
     }
-    
     `
 };
