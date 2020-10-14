f5483bd63e4a46bce365dc08c4f02e46f439fe4c
Jefferson Andrés Rodríguez Sánchez <jefferson_rodriguez@registropmanta.local>
2020-10-14 17:01:24 -0500

    ahora si este si es jj

--- src/querys/querys.js
+++ src/querys/querys.js
@@ -1,7 +1,7 @@
 module.exports = {
     TiposServicios: `query rep_tiposServicios($fechaInicio: timestamp!, $fechaFin: timestamp!) {
       TipoServicio(where: {OrdenTrabajo_Detalles: {ProformaFacturaDetalles: {FacturadoEn: {_gte: $fechaInicio, _lte: $fechaFin}}}}) {
-    		TpServicio
+        TpServicio
         creacion: OrdenTrabajo_Detalles_aggregate(where: {ProformaFacturaDetalles: {FacturadoEn: {_gte: $fechaInicio, _lte: $fechaFin}}, OrdenTrabajo_Cabecera: {Estado: {_eq: 0}}}) {
           aggregate {
             sum {
@@ -65,15 +65,140 @@ module.exports = {
               AmountInvoiced
             }
           }
-          nodes {
+          nodes{
             OID
             Cantidad
-            usuarioByIduserasignado {
-              Cedula
-              Nombres
-              Apellidos
+            TipoTramite{
+              Codigo
+              DscaTipoTramite
             }
           }
+        }
+      }
+    }
+    `,
+    DetallesTareas: `query lista_servicios($fechaInicio: timestamp!, $fechaFin: timestamp!, $cedula: String!) {
+      TipoServicio(where: {OrdenTrabajo_Detalles: {usuarioByIduserasignado: {Cedula: {_eq: $cedula}}, Usuario_OTs: {FechaRegistro: {_gte: $fechaInicio, _lte: $fechaFin}}}}) {
+        TpServicio
+        OrdenTrabajo_Detalles_aggregate(where: {Usuario_OTs: {FechaRegistro: {_gte: $fechaInicio, _lte: $fechaFin}}, usuarioByIduserasignado: {Cedula: {_eq: $cedula}}}) {
+          aggregate {
+            sum {
+              Cantidad
+            }
+          }
+        }
+        OrdenTrabajo_Detalles(where: {Usuario_OTs: {FechaRegistro: {_gte: $fechaInicio, _lte: $fechaFin}}, usuarioByIduserasignado: {Cedula: {_eq: $cedula}}}) {
+          TipoServicio {
+            TpServicio
+          }
+          TipoTramite {
+            DscaTipoTramite
+          }
+          Observacion
+          StatusOT
+          CreadoEn
+          FechaInscripcion
+          FechaInicioTrabajo
+          FechaEstimadaEntrega
+          FechaPospuestaEntrega
+          FechaRealEntrega
+          Avaluo
+          AmountInvoiced
+          FojasAdc
+          Cantidad
+          usuarioByIduserasignado{
+            Nombres
+            Apellidos
+          }
+          usuarioByCreadopor {
+            Nombres
+            Apellidos
+          }
+          Usuario_OTs {
+            FechaFinalizacion
+          }
+          OrdenTrabajo_Cabecera {
+            NroOrden
+            Estado
+            clienteByClientefactura {
+              Nombres
+              Apellidos
+              NombreEmpresa
+            }
+          }
+        }
+      }
+    }
+    `,
+    DetallesExcel: `query detalles_excel($fechaInicio: timestamp!, $fechaFin: timestamp!, $status: [Int!]) {
+      OrdenTrabajo_Detalle(where: {CreadoEn: {_gte: $fechaInicio, _lte: $fechaFin}, StatusOT: {_in: $status}}) {
+        TipoServicio {
+          TpServicio
+        }
+        TipoTramite {
+          DscaTipoTramite
+        }
+        StatusOT
+        CreadoEn
+        FechaInscripcion
+        FechaInicioTrabajo
+        FechaEstimadaEntrega
+        FechaPospuestaEntrega
+        FechaRealEntrega
+        Avaluo
+        AmountInvoiced
+        FojasAdc
+        Cantidad
+        usuarioByIduserasignado {
+          Nombres
+          Apellidos
+        }
+        usuarioByCreadopor {
+          Nombres
+          Apellidos
+        }
+        Usuario_OTs {
+          FechaRegistro
+          FechaFinalizacion
+        }
+        OrdenTrabajo_Cabecera {
+          ExcentoCobro
+          OrdenGubernamental
+          NroOrden
+          Estado
+          clienteByClientefactura {
+            NombreEmpresa
+            RepresentanteLegal
+            TerceraEdad
+            Nombres
+            Apellidos
+            DiscapacidadValidaParaDescuento
+          }
+        }
+        Observacion
+      }
+      OrdenTrabajo_Detalle_aggregate(where: {CreadoEn: {_gte: $fechaInicio, _lte: $fechaFin}, StatusOT: {_in: $status}}) {
+        aggregate {
+          count
+        }
+      }
+    }
+    `,
+    eliminarLinderos: `
+    query del_linderos {
+      Ficha_Registral(where: {Lindero: {_like: "%SOLVENCIA%"}}, limit: 1000) {
+        OID
+        Lindero
+      }
+    }    
+    `,
+    mutacion: `
+    mutation($OID: Int!, $lindero: String!){
+      update_Ficha_Registral(where:{OID:{_eq: $OID}}, 
+        _set:{Lindero: $lindero}){
+        returning{
+          OID
+          Lindero
         }
       }
     }
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const { GraphQLClient } = require("graphql-request");
-const { TiposServicios } = require('../src/querys/querys')
-const endpoint = "http://192.168.104.200:8080/v1/graphql";
-const {ordenar} = require("./utils/helpers")
+const { TiposServicios, DetallesTareas, DetallesExcel, eliminarLinderos,mutacion } = require('../src/querys/querys')
+const endpoint = "http://192.168.105.34:8080/v1/graphql"
+const moment = require("../node_modules/moment")
 const typeDefs = gql`
   type typeOfService {
     Nombre: String
@@ -17,16 +17,87 @@ const typeDefs = gql`
     Finalizada: Int
     Total: Int
     totalRecaudado: Float
-    Usuarios: [typeUser]
+    Tareas: [typeTask]
   }
-  type typeUser {
-    OID: [Int]
-    Cedula: String
-    Nombre: String
+  type typeTask {
     Cantidad: Int
+    Codigo: String
+    DscaTipoTramite: String
+    OID: [Int]
+  }
+  type taskList{
+    TpServicio: String
+    DscaTipoTramite: String
+    StatusOT: Int
+    CreadoEn: String
+    FechaInscripcion: String
+    FechaInicioTrabajo: String
+    FechaEstimadaEntrega: String
+    FechaPospuestaEntrega: String
+    FechaRealEntrega: String
+    Avaluo: Int
+    AmountInvoiced: Float
+    FojasAdc: Int
+    Cantidad: Int
+    NombresAsi: String
+    NombresCre: String
+    FechaFinalizacion: String
+    NroOrden: String
+    Estado: Int
+    Nombres: String
+    Observacion: String
+    Estilo: typeStyle
+  }
+  type typeStyle{
+    isGreen: Boolean
+    isBlue: Boolean
+    isRed: Boolean
+    isYellow: Boolean
+  }
+  type serviceList{
+    TpServicio: String
+    Cantidad: Int
+    tareas: [taskList]
+  }
+  type dataEcxel{
+    Nro: String
+    TpServicio: String
+    fRegistro: String
+    Etarea: String
+    Asignado: String
+    tpTramite: String
+    NroOrden: String
+    FEfin: String
+    Observacion: String
+    CreadoEn: String
+    FInscripcion: String
+    FInicioTrabajo: String
+    FPostpuestaEntrega: String
+    FRealEntrega: String
+    Avaluo: String
+    Monto: String
+    FojasAdc: String
+    Cantidad: String
+    CreadoPor: String
+    FFinalizacion: String
+    ExcentoCobro: String
+    OrdenGubernamental: String
+    EstadoOrden: String
+    Cliente: String
+    Empresa: String
+    RepLegal: String
+    TerceraEdad: String
+    Discapacidad: String
+  }
+  type linderos{
+    OID: Int
+    lindero: String
   }
   type Query {
     rep_tiposServicios(fechaInicio: String!, fechaFin: String!): [typeOfService]
+    lista_servicios(fechaInicio: String!, fechaFin: String!, cedula: String!): [serviceList]
+    detalles_excel(fechaInicio: String!, fechaFin: String!, status: [Int]): [dataEcxel]
+    del_linderos: [linderos]
   }
 `;
 
@@ -54,7 +125,7 @@ const resolvers = {
           Finalizada: 0,
           Total: 0,
           totalRecaudado: 0,
-          Usuarios: []
+          Tareas: []
         }
         data.TipoServicio.forEach(servicio=>{
           var aux = {
@@ -69,7 +140,7 @@ const resolvers = {
             Finalizada: servicio.finalizada.aggregate.sum.Cantidad == null ? 0 : servicio.finalizada.aggregate.sum.Cantidad,
             Total: servicio.total.aggregate.sum.Cantidad == null ? 0 : servicio.total.aggregate.sum.Cantidad,
             totalRecaudado: servicio.total.aggregate.sum.AmountInvoiced == null ? 0 : servicio.total.aggregate.sum.AmountInvoiced,
-            Usuarios: null
+            Tareas: null
           }
           total.Creacion += aux.Creacion
           total.Abierta += aux.Abierta
@@ -81,38 +152,29 @@ const resolvers = {
           total.Finalizada += aux.Finalizada
           total.Total += aux.Total
           total.totalRecaudado += aux.totalRecaudado
-          servicio.total.nodes.forEach(usuario=>{
-            if(usuario.usuarioByIduserasignado != null){
-              var auxUsuarios={
-                OID: [],
-                Cedula: usuario.usuarioByIduserasignado.Cedula,
-                Nombre: usuario.usuarioByIduserasignado.Apellidos+" "+usuario.usuarioByIduserasignado.Nombres,
-                Cantidad: usuario.Cantidad
-              }
-            }else{
-              var auxUsuarios={
-                OID: [],
-                Cedula: "",
-                Nombre: "SIN ASIGNAR",
-                Cantidad: usuario.Cantidad
-              }
+          servicio.total.nodes.forEach(tarea=>{
+            var auxTareas={
+              OID: [],
+              Codigo: tarea.TipoTramite.Codigo,
+              DscaTipoTramite: tarea.TipoTramite.DscaTipoTramite,
+              Cantidad: tarea.Cantidad
             }
-            auxUsuarios.OID.push(usuario.OID)
-            if(aux.Usuarios == null){
-              aux.Usuarios= [];
-              aux.Usuarios.push(auxUsuarios);
+            auxTareas.OID.push(tarea.OID)
+            if(aux.Tareas == null){
+              aux.Tareas= [];
+              aux.Tareas.push(auxTareas);
             }else{
               var found=false;
-              aux.Usuarios.forEach(usu=>{
-                if(auxUsuarios.Cedula == usu.Cedula){
-                  usu.Cantidad+= auxUsuarios.Cantidad;
-                  usu.OID.push(usuario.OID);
+              aux.Tareas.forEach(tar=>{
+                if(auxTareas.Codigo == tar.Codigo){
+                  tar.Cantidad+= auxTareas.Cantidad;
+                  tar.OID.push(tarea.OID);
                   found=true;
                   return;
                 }
               });
               if(found==false){
-                aux.Usuarios.push(auxUsuarios);
+                aux.Tareas.push(auxTareas);
               }
             }
           });
@@ -129,12 +191,12 @@ const resolvers = {
             return 0;
         });
           listaServicios.forEach(lista=>{
-            if(lista.Usuarios != null){
-              lista.Usuarios.sort(function(a,b){
-                if (a.Nombre > b.Nombre){
+            if(lista.Tareas != null){
+              lista.Tareas.sort(function(a,b){
+                if (a.DscaTipoTramite > b.DscaTipoTramite){
                     return 1;
                 }
-                if (a.Nombre < b.Nombre){
+                if (a.DscaTipoTramite < b.DscaTipoTramite){
                     return -1;
                 }
                 return 0;
@@ -144,6 +206,197 @@ const resolvers = {
           listaServicios.push(total);
         }
         return listaServicios;
+      } catch (e) {
+        console.log(e);
+        return null;
+      }
+    },
+    lista_servicios: async (parent, args, context) =>{
+      try{
+        var { fechaInicio, fechaFin, cedula} = args
+        const variables = {
+          fechaInicio,
+          fechaFin,
+          cedula
+        }
+        const graphQLClient = new GraphQLClient(endpoint);
+        const data = await graphQLClient.request(DetallesTareas, variables);
+        var listaDetalles = []
+        data.TipoServicio.forEach(servicio=>{
+          var auxD= {
+            TpServicio: servicio.TpServicio,
+            Cantidad: servicio.OrdenTrabajo_Detalles_aggregate.aggregate.sum.Cantidad,
+            tareas: []
+          }
+          servicio.OrdenTrabajo_Detalles.forEach(ordenes=>{
+            var auxO= {
+              TpServicio: ordenes.TipoServicio.TpServicio,
+              DscaTipoTramite: ordenes.TipoTramite.DscaTipoTramite,
+              StatusOT: ordenes.StatusOT,
+              CreadoEn: ordenes.CreadoEn,
+              FechaInscripcion: ordenes.FechaInscripcion,
+              FechaInicioTrabajo: ordenes.FechaInicioTrabajo,
+              FechaEstimadaEntrega: ordenes.FechaEstimadaEntrega,
+              FechaPospuestaEntrega: ordenes.FechaPospuestaEntrega,
+              FechaRealEntrega: ordenes.FechaRealEntrega,
+              Avaluo: ordenes.Avaluo,
+              AmountInvoiced: ordenes.AmountInvoiced,
+              FojasAdc: ordenes.FojasAdc,
+              Cantidad: ordenes.Cantidad,
+              NombresAsi: ordenes.usuarioByIduserasignado.Apellidos+" "+ordenes.usuarioByIduserasignado.Nombres,
+              NombresCre: ordenes.usuarioByCreadopor.Apellidos+" "+ordenes.usuarioByCreadopor.Nombres,
+              FechaFinalizacion: ordenes.Usuario_OTs[0].FechaFinalizacion,
+              NroOrden: ordenes.OrdenTrabajo_Cabecera.NroOrden,
+              Estado: ordenes.OrdenTrabajo_Cabecera.Estado,
+              Nombres: ordenes.OrdenTrabajo_Cabecera.clienteByClientefactura.Apellidos+" "+ordenes.OrdenTrabajo_Cabecera.clienteByClientefactura.Nombres+ordenes.OrdenTrabajo_Cabecera.clienteByClientefactura.NombreEmpresa,
+              Observacion: ordenes.Observacion,
+              Estilo: {
+                isBlue: false,
+                isRed: false,
+                isGreen: false,
+                isYellow: false
+              }
+            }
+            var inicio = moment(new Date())
+            var fin = moment(auxO.FechaEstimadaEntrega, 'YYYY-MM-DD h:m:s').toDate()
+            if (auxO.StatusOT !== 5) {
+              if (auxO.FechaEstimadaEntrega == null || auxO.StatusOT === 6) {
+                auxO.Estilo.isBlue = true
+              } else {
+                if ((fin - inicio) / (1000 * 60 * 60 * 24) > 1) {
+                  auxO.Estilo.isGreen = true
+                } else if ((fin - inicio) / (1000 * 60 * 60) > 2) {
+                  auxO.Estilo.isYellow = true
+                } else {
+                  auxO.Estilo.isRed = true
+                }
+              }
+            }
+            auxD.tareas.push(auxO);
+          });
+          listaDetalles.push(auxD);
+        });
+        if(listaDetalles != null){
+          listaDetalles.sort(function(a,b){
+            if (a.TpServicio > b.TpServicio){
+                return 1;
+            }
+            if (a.TpServicio < b.TpServicio){
+                return -1;
+            }
+            return 0;
+          });
+          listaDetalles.forEach(lista=>{
+            if(lista.tareas != null){
+              lista.tareas.sort(function(a,b){
+                if (a.FechaEstimadaEntrega > b.FechaEstimadaEntrega){
+                    return 1;
+                }
+                if (a.FechaEstimadaEntrega < b.FechaEstimadaEntrega){
+                    return -1;
+                }
+                return 0;
+            });
+            }
+          })
+        }
+        return listaDetalles;
+      } catch (e) {
+        console.log(e);
+        return null;
+      }
+    },
+    detalles_excel:async (parent, args, context) =>{
+      try{
+        var { fechaInicio, fechaFin, status } = args
+        const variables = {
+          fechaInicio,
+          fechaFin,
+          status
+        }
+        const graphQLClient = new GraphQLClient(endpoint);
+        const data = await graphQLClient.request(DetallesExcel, variables);
+        var count = 0;
+        var tasksStatus = [
+          'No Iniciado',
+          'Rev Jurídico',
+          'Rev Jurídico C',
+          'En Proceso',
+          'Por Firmar',
+          'Completado',
+          'Pendiente',
+          'Anulado'
+        ]
+        var rows = data.OrdenTrabajo_Detalle.map(det => {
+          if(det.OrdenTrabajo_Cabecera == null){
+            det.OrdenTrabajo_Cabecera = {
+              ExcentoCobro: null,
+              OrdenGubernamental: null,
+              NroOrden: null,
+              Estado: null,
+              clienteByClientefactura: {
+                NombreEmpresa: null,
+                RepresentanteLegal: null,
+                TerceraEdad: null,
+                Nombres: null,
+                Apellidos: null,
+                DiscapacidadValidaParaDescuento: null
+              }
+            }
+          }
+          var aux = {
+            Nro: count++,
+            TpServicio: det.TipoServicio == null ? '' : det.TipoServicio.TpServicio,
+            fRegistro: det.Usuario_OTs.length == 0 ? '' : moment(det.Usuario_OTs[0].FechaRegistro).format('YYYY-MM-DD hh:mm:ss'),
+            Etarea: tasksStatus[det.StatusOT] == null ? '' : tasksStatus[det.StatusOT],
+            Asignado: det.usuarioByIduserasignado == null ? '' : `${det.usuarioByIduserasignado.Nombres} ${det.usuarioByIduserasignado.Apellidos}`,
+            tpTramite: det.TipoTramite == null ? '' : det.TipoTramite.DscaTipoTramite,
+            NroOrden: det.OrdenTrabajo_Cabecera.NroOrden == null ? '' : det.OrdenTrabajo_Cabecera.NroOrden,
+            FEfin: det.FechaEstimadaEntrega == null ? '' : moment(det.FechaEstimadaEntrega).format('YYYY-MM-DD hh:mm:ss'),
+            Observacion: det.Observacion == null ? '' : det.Observacion,
+            CreadoEn: det.CreadoEn == null ? '' : moment(det.CreadoEn).format('YYYY-MM-DD hh:mm:ss'),
+            FInscripcion: det.FechaInscripcion == null ? '' : moment(det.FechaInscripcion).format('YYYY-MM-DD hh:mm:ss'),
+            FInicioTrabajo: det.FechaInicioTrabajo == null ? '' : moment(det.FechaInicioTrabajo).format('YYYY-MM-DD hh:mm:ss'),
+            FPostpuestaEntrega: det.FechaPospuestaEntrega == null ? '' : moment(det.FechaPospuestaEntrega).format('YYYY-MM-DD hh:mm:ss'),
+            FRealEntrega: det.FechaRealEntrega == null ? '' : moment(det.FechaRealEntrega).format('YYYY-MM-DD hh:mm:ss'),
+            Avaluo: det.Avaluo == null ? '' : det.Avaluo,
+            Monto: det.AmountInvoiced == null ? '' : det.AmountInvoiced,
+            FojasAdc: det.FojasAdc == null ? '' : det.FojasAdc,
+            Cantidad: det.FojasAdc == null ? '' : det.FojasAdc,
+            CreadoPor: det.usuarioByCreadopor == null ? '' : `${det.usuarioByCreadopor.Nombres} ${det.usuarioByCreadopor.Apellidos}`,
+            FFinalizacion: det.Usuario_OTs.length == 0 ? '' : moment(det.Usuario_OTs[0].FechaFinalizacion).format('YYYY-MM-DD hh:mm:ss'),
+            ExcentoCobro: det.OrdenTrabajo_Cabecera.ExcentoCobro == null ? '' : det.OrdenTrabajo_Cabecera.ExcentoCobro,
+            OrdenGubernamental: det.OrdenTrabajo_Cabecera.OrdenGubernamental == null ? '' : det.OrdenTrabajo_Cabecera.OrdenGubernamental,
+            EstadoOrden: det.OrdenTrabajo_Cabecera.Estado == null ? '' : det.OrdenTrabajo_Cabecera.Estado,
+            Cliente: det.OrdenTrabajo_Cabecera.clienteByClientefactura.Nombres == null ? '' : `${det.OrdenTrabajo_Cabecera.clienteByClientefactura.Nombres} ${det.OrdenTrabajo_Cabecera.clienteByClientefactura.Apellidos}`,
+            Empresa: det.OrdenTrabajo_Cabecera.clienteByClientefactura.NombreEmpresa == null ? '' : det.OrdenTrabajo_Cabecera.clienteByClientefactura.NombreEmpresa,
+            RepLegal: det.OrdenTrabajo_Cabecera.clienteByClientefactura.RepresentanteLegal == null ? '' : det.OrdenTrabajo_Cabecera.clienteByClientefactura.RepresentanteLegal,
+            TerceraEdad: det.OrdenTrabajo_Cabecera.clienteByClientefactura.TerceraEdad == null ? '' : det.OrdenTrabajo_Cabecera.clienteByClientefactura.TerceraEdad,
+            Discapacidad: det.OrdenTrabajo_Cabecera.clienteByClientefactura.DiscapacidadValidaParaDescuento == null ? '' : det.OrdenTrabajo_Cabecera.clienteByClientefactura.DiscapacidadValidaParaDescuento
+          }
+          return aux
+        })
+        return rows;
+      } catch (e) {
+        console.log(e);
+        return null;
+      }
+    },
+    del_linderos: async (parent, args, context) =>{
+      try{
+        const graphQLClient = new GraphQLClient(endpoint);
+        const data = await graphQLClient.request(eliminarLinderos);
+        var respuesta= []
+        data.Ficha_Registral.forEach(rep=>{
+          var res = rep.Lindero.split("SOLVENCVIA");
+          const aux ={
+            OID: rep.OID,
+            lindero: res[0]
+          }
+          graphQLClient.request(mutacion, aux);
+          respuesta.push(aux);
+        });       
+        return respuesta;
       } catch (e) {
         console.log(e);
         return null;
